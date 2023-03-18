e8a65510910f29342da77ba4c09b2930398713c3
Lassi Rissanen <82208000+lassirissanen@users.noreply.github.com>
2023-03-18 10:50:25 +0200

    Create backend-service.js

---
+++ frontend/src/service/backend-service.js
@@ -0,0 +1,43 @@
+const baseUrl = 'http://localhost:5000';
+
+async function get(path, message) {
+    const url=`${baseUrl}${path}`;
+    const data = {
+        "message": message
+    };
+    console.log(data);
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        return response.json();
+    } catch (error) {
+        console.log(error)
+    }
+    return null;
+}
+
+async function getTensorflowClassification(message) {
+    const path="/classify-1";
+    return await get(path, message);
+}
+
+async function getCombinedClassification(message) {
+    const path = "/classify-2";
+    return await get(path, message);
+}
+
+async function getOpenAIClassification(message) {
+    const path="/classify-3";
+    return await get(path, message);
+}
+
+export {
+    getTensorflowClassification,
+    getCombinedClassification,
+    getOpenAIClassification
+}
