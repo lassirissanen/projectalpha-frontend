89a17efd77e48d56207658f800329c99a316dddd
Lassi Rissanen <82208000+lassirissanen@users.noreply.github.com>
2023-03-19 16:32:19 +0200

    fix merge conflicts

--- frontend/src/chat/Message.jsx
+++ frontend/src/chat/Message.jsx
@@ -3,20 +3,9 @@ export const Message = ({message}) => {
     // the currently logged in user
 
 	function isMessageFromUser() {
-<<<<<<< HEAD
         return message.user === "krister";
     }
 
-=======
-
-        //console.log(message)
-        //console.log(message.user)
-        return message.user === "krister";
-    }
-
-    //console.log(isMessageFromUser())
-
->>>>>>> d398bedcd3cb0586e450d34ae64c90f5985e6fc0
 	return (
 		<div class={`${isMessageFromUser() ? "place-self-end" : "place-self-start"} space-y-2`}> 
             <div class={`bg-light-blue text-white p-5 rounded-2xl ${isMessageFromUser() ? "rounded-tr-none" : "rounded-tl-none"}`}>
